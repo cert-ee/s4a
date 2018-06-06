96c3a167a5b885cc69b745cf749db9dabbfd18be
Andres Elliku <andres@cert.ee>
2018-06-06 11:14:44 +0300

    Only run states if autoupgrade true

--- saltstack/salt/detector/files/cron/auto_detector_states.py
+++ saltstack/salt/detector/files/cron/auto_detector_states.py
@@ -20,21 +20,32 @@ def main():
     caller = salt.client.Caller()
     api = caller.cmd('pillar.get', 'detector:api', {'host': '127.0.0.1', 'port': 4000})
     api_url = 'http://{0}:{1}/api/components'.format(api['host'], api['port'])
+    api_upgrade = 'http://{host}:{port}/api/components/autoupgrade'.format(api['host'], api['port'])
 
     try:
-        components = requests.get(api_url)
+        autoupgrade = requests.get(api_upgrade)
     except requests.exceptions.RequestException as e:
-        logging.error('Could not communicate with detector API(%s) because: %s', api_url, e)
+        logging.error('Could not communicate with detector API(%s) because: %s', api_upgrade, e)
         sys.exit(1)
+    
+    # If auto upgrade is set to true in the webinterface then run states
+    if autoupgrade.json()['enabled']:
+        try:
+            components = requests.get(api_url)
+        except requests.exceptions.RequestException as e:
+            logging.error('Could not communicate with detector API(%s) because: %s', api_url, e)
+            sys.exit(1)
 
-    for comp in components.json():
-        if comp['enabled'] and comp['installed'] and comp['name'] != 'evebox-agent':
-            logging.info('Running state detector/%s', comp['name'])
-            output = caller.cmd('state.apply', 'detector/{0}'.format(comp['name']))
-            logging.info('Ran state detector/%s with result: %s', comp['name'], output)
+        for comp in components.json():
+            if comp['enabled'] and comp['installed'] and comp['name'] != 'evebox-agent':
+                logging.info('Running state detector/%s', comp['name'])
+                output = caller.cmd('state.apply', 'detector/{0}'.format(comp['name']))
+                logging.info('Ran state detector/%s with result: %s', comp['name'], output)
 
-    cron_output = caller.cmd('state.apply', 'detector/cron')
-    logging.info('Ran state detector/cron with result: %s', cron_output)
+        cron_output = caller.cmd('state.apply', 'detector/cron')
+        logging.info('Ran state detector/cron with result: %s', cron_output)
+    else:
+        logging.info('Auto upgrade is not enabled in the web interface. Not doing anything')
 
 
 if __name__ == '__main__':
